de9af4b01d0373ac99a9d4e71b6608780788ab48
QuantiKa14 <Quantika14@users.noreply.github.com>
2019-03-04 22:07:48 +0100

    Add files via upload

---
+++ modules/control.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+import sys, os
+
+def systemDetect():
+    sistemaop = sys.platform
+    return sistemaop
+
+def countFiles_data():
+    if "win" in systemDetect():
+        print "[!][WARNING][>] Recomendamos el uso de Linux..."
+    if "linux" in systemDetect():
+        f_count = os.popen("ls -A | wc -l")
+        if f_count < 10000:
+            print "[*][LOCAL][>] Actualmente su low data es de: " + str(f_count)
+        else:
+            print "[*][LOCAL][>] Actualmente su big data es de: " + str(f_count)
---
+++ modules/er.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+def replace_acentos(cadena):
+    cadena = cadena.upper()
+    cadena=cadena.replace('Á','A');
+    cadena=cadena.replace('É','E');
+    cadena=cadena.replace('Í','I');
+    cadena=cadena.replace('Ó','O');
+    cadena=cadena.replace('Ú','U');
+    cadena=cadena.replace('Ñ','N');
+    cadena=cadena.replace('Ä','A');
+    cadena=cadena.replace('Ë','E');
+    cadena=cadena.replace('Ï','I');
+    cadena=cadena.replace('Ö','O');
+    cadena=cadena.replace('Ü','U');
+    return cadena.lower()
+
+def replace_letras_raras(cadena):
+    cadena = cadena.upper()
+    cadena = cadena.replace("Ñ", "N")
+    return cadena.lower()
