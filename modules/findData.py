f10762b8c67c9e54eb948ceb50380689e2931237
QuantiKa14 <Quantika14@users.noreply.github.com>
2019-03-07 15:56:01 +0100

    Add files via upload

--- modules/findData.py
+++ modules/findData.py
@@ -3,34 +3,37 @@
 
 import modules.control as control
 import os
+from subprocess import call
+
+def clean_empresa(empresa):
+    empresa = empresa.split(" ")
+    e_2 = empresa[1]
+    return empresa[0]+ " " + e_2[0]
 
 def search_and_find_data(n, ap1, ap2):
     if "win" in control.systemDetect():
         print "[!][WARNING][>] Esta funcionalidad solo está disponible en Linux..."
-    if "linux" in control.systemDetect():
 
+    if "linux" in control.systemDetect():
+        print ""
+        print "[---------------------BÚSQUEDA LOCAL DE ARCHIVOS-----------------------------]"
+        print "|----[INFO][>] Thread 1: puede tardar varios minutos..."
+        print ""
 
         #Buscamos los dos apellidos en minusculas
-        print "|----[INFO][ARCHIVOS][DATA LOCAL][>] Apellidos en minus..."
-        command1 = "pdfgrep '" + ap1.lower() + " " + ap2.lower() + "' -R"
-        exe_find = os.system(command1)
-        print exe_find
+        print "|----[INFO][ARCHIVOS][DATA LOCAL][>] Buscando por apellidos..."
+        target = "'" + ap1.lower() + " " + ap2.lower() + "'"
+        os.system("pdfgrep " + target + " -ni data/ -r")
 
-        #Buscamos los dos apellidos y el nombre en minusculas
-        print "|----[INFO][ARCHIVOS][DATA LOCAL][>] Apellidos y nombre en minus..."
-        command2 = "pdfgrep '" + ap1.lower() + " " + ap2.lower() + " " + n.lower() + "' -R"
-        exe_find = os.system(command2)
-        print exe_find
 
         #Buscamos los dos apellidos en mayusculas
-        print "|----[INFO][ARCHIVOS][DATA LOCAL][>] Apellidos en Mayus"
-        command1 = "pdfgrep '" + ap1.upper() + " " + ap2.upper() + "' -R"
-        exe_find = os.system(command1)
-        print exe_find
+        print "|----[INFO][ARCHIVOS][DATA LOCAL][>] Buscando por apellidos mayusculas..."
+        target = "'" + ap1.upper() + " " + ap2.upper() + "'"
+        os.system("pdfgrep " + target + " -ni data/ -r")
 
-        #Buscamos los dos apellidos y el nombre en mayusculas
-        print "|----[INFO][ARCHIVOS][DATA LOCAL][>] Apellidos y nombre en Mayus"
-        command2 = "pdfgrep '" + ap1.upper() + " " + ap2.upper() + " " + n.upper() + "' -R"
-        exe_find = os.system(command2)
-        print exe_find
+def search_adjudicaciones(empresa):
+    #Buscamos la empresa en adjudicaciones
+    empresa = clean_empresa(empresa)
+    os.system("pdfgrep '" + empresa + "' -ni data/Adjudicaciones/ -r")
+
 
