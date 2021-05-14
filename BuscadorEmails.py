8e421239f607d535c5f03ac3e9ee0431bad9f9d1
QuantiKa14 <Quantika14@users.noreply.github.com>
2021-05-14 14:38:45 +0200

    New version and fixed bugs

--- BuscadorEmails.py
+++ BuscadorEmails.py
@@ -29,8 +29,9 @@ from validate_email import validate_email
 import csv, operator
 
 import modules.config as C
-import modules.intelx_ as iX
+#import modules.intelx_ as iX
 import modules.er as R
+import modules.dic as dic
 
 
 br = mechanize.Browser()
@@ -42,13 +43,26 @@ br.set_handle_robots( False )
 br.set_handle_refresh( mechanize._http.HTTPRefreshProcessor(), max_time = 1 ) 
 br.addheaders = [ ( 'User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1' ) ] 
 
-def intelX(target):
-	try:
-		iX.get_pastes(target)
-		iX.get_leaks(target)
-		iX.get_darknet(target)
-	except:
-		print("|----[WARNING][>] Insert API key in key.py...")
+def checkEmails_(email):
+
+	username = email.split("@")
+	username = username[0]
+	print(f"|--[INFO][>] Now we are going to investigate if the username ({username}) has other emails with different domains...")
+	for domain in dic.commonMails:
+		email = username + "@" + domain
+		is_valid = validate_email(email)
+		if is_valid:
+			print(C.colores.alert + f"|--[CHECK EMAIL][>] The email ({email}) exists!" + C.colores.normal)
+		else:
+			print(f"|--[CHECK EMAIL][>] The email ({email}) does not exists.")
+
+# def intelX(target):
+# 	try:
+# 		iX.get_pastes(target)
+# 		iX.get_leaks(target)
+# 		iX.get_darknet(target)
+# 	except:
+# 		print("|----[WARNING][>] Insert API key in key.py...")
 
 def get_usernameEmail(email):
 	email = email.split("@")
@@ -116,23 +130,6 @@ def check_haveibeenpwned(email):
 		print("|--[INFO][HAVEIBEENPWNED][>] Your email appear in leaks...")
 	else:
 		print("|--[INFO][HAVEIBEENPWNED][>] Your email doesn't appear in leaks...")
-
-def check_facebook(email):
-
-	r = br.open('https://mbasic.facebook.com/')
-	br.select_form(nr=0)
-	br.form["email"] = email
-	br.form["pass"] = "123456"
-	br.submit()
-	respuestaURL = br.response().geturl()
-
-	html =  br.response().read()
-	soup = BeautifulSoup(html, "html.parser")
-	div = soup.find("div",{"id":"login_error"})
-	if "ninguna" in R.remove_tags(str(div)):
-		print("|--[INFO][FACEBOOK][CHECK][>] Account doesn't exist...")
-	else:
-		print("|--[INFO][FACEBOOK][CHECK][>] The account exist...")
 
 
 def check_emailrep(email):
@@ -206,7 +203,7 @@ def banner():
     - Verify LinkedIn, WordPress, Spotify, Tumblr, Netflix, Pinterest, 
     - Pastebin
 -------------------------------------------------------------------------------------
-Date version: 09/01/2017 | Version: 1.0
+Date version: 09/01/2017 | Version: 1.5.3
 Date latest version: 21/01/2017 | Version: 1.0.1
 Date latest version: 27/07/2018 | Version: 1.0.9
 Date latest version: 31/07/2018 | Version: 1.2.1
@@ -214,6 +211,7 @@ Date latest version: 30/01/2019 | Version: 1.2.3
 Date latest version: 30/08/2019 | Version: 1.3.0
 Date latest version: 30/09/2019 | Version: 1.4.1
 Date latest version: 09/03/2020 | Version: 1.5.1
+Date latest version: 14/05/2021 | Version: 1.5.3
 -------------------------------------------------------------------------------------
 """)
 
@@ -236,19 +234,29 @@ def attack(email):
 	email = email.replace("\n", "")
 
 	if email:
+		EMAIL_REGEX = r'(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)'
+		if not re.match(EMAIL_REGEX, email):
+			print(C.colores.alert + "Email format is wrong!" + C.colores.normal)
+			exit()
+
 		print("[INFO][TARGET][>] Hello, " + email + " is correct.")
 		
 		print("[INFO][TARGET][>] " + email)
 		print("|--[INFO][EMAIL][>] Email validated...")
 
 		#CALL THE ACTION
-		intelX(email)
+		#intelX(email)
+		x = input("|--[QUESTION][>] Do you want to investigate other emails with the same username? Y/n: ")
+		if x == "Y" or x == "y":
+			checkEmails_(email)
+		else:
+			print("")
+
 		check_emailrep(email)
 		check_AccountTwitter(email)
 		check_wordpress(email)
 		check_netflix(email)
 		check_haveibeenpwned(email)
-		check_facebook(email)
 
 	else:
 		print("[INFO][TARGET][>] Hello, " + email + " doesn't a email. Try again!'")
